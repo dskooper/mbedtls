72dece6530b6ec9b4c6ff3f1de7e32a44bff43d5
kooper! <ds.kooper@outlook.com>
2026-05-21 12:46:30 +0100

    Fix wrong SDK being called
    Purplecord uses the iPhoneOS3.1.3 SDK
    The iphoneos3.cmake file specifically requested the iPhoneOS3.0 SDK
    oops
    
    Signed-off-by: kooper! <ds.kooper@outlook.com>

--- iphoneos3.cmake
+++ iphoneos3.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_VERSION 3.0)
-set(CMAKE_OSX_SYSROOT $ENV{THEOS}/sdks/iPhoneOS3.0.sdk)
+set(CMAKE_OSX_SYSROOT $ENV{THEOS}/sdks/iPhoneOS3.1.3.sdk)
 
 set(CMAKE_C_COMPILER $ENV{THEOS}/toolchain/linux/iphone/bin/clang)
 set(CMAKE_CXX_COMPILER $ENV{THEOS}/toolchain/linux/iphone/bin/clang)
@@ -8,7 +8,7 @@ set(CMAKE_LINKER $ENV{THEOS}/toolchain/linux/iphone/bin/clang)
 set(CMAKE_AR $ENV{THEOS}/toolchain/linux/iphone/bin/llvm-ar)
 set(CMAKE_RANLIB $ENV{THEOS}/toolchain/linux/iphone/bin/llvm-ranlib)
 
-set(CMAKE_C_FLAGS "-target armv6-apple-darwin9 -isysroot $ENV{THEOS}/sdks/iPhoneOS3.0.sdk -Os -DOLD_IPHONE")
+set(CMAKE_C_FLAGS "-target armv6-apple-darwin9 -isysroot $ENV{THEOS}/sdks/iPhoneOS3.1.3.sdk -Os -DOLD_IPHONE")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
